cfce5968ce5085862d7d180399485cb5c44f37e3
Daniel Shimield <dxshim90@gmail.com>
2019-02-18 15:09:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,5 @@
 def reformat_languages(languages)
+<<<<<<< HEAD
   new_languages = {}
   languages.each do |style, language|
     language.each do |language_name, attribute|
@@ -11,4 +12,18 @@ def reformat_languages(languages)
   end
   new_languages[:javascript][:style] << :oo
   new_languages
+=======
+  new_hash = {}
+  new_hash.each do |style, language|
+    new_hash.each do |language_name, attribute|
+      if new_hash[language_name] == nil
+          new_hash[language_name] = attribute
+        end
+      new_hash[language_name][:style] = []
+      new_hash[language_name][:style] << style
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
+>>>>>>> 407a4e141939eb53d8f554043eeb06e24a6cf174
 end
